b38a71321f11eb929bf4581bc1a51e3b2131a240
Ivan Andreev <iivvaann.andreev@gmail.com>
2020-02-20 20:36:11 +0000

    update

---
+++ runall.bat
@@ -0,0 +1,7 @@
+@echo off
+cmd /c gh.exe < inputs\a_example.txt > outputs\a_example.txt
+cmd /c gh.exe < inputs\b_read_on.txt > outputs\b_read_on.txt
+cmd /c gh.exe < inputs\c_incunabula.txt > outputs\c_incunabula.txt
+cmd /c gh.exe < inputs\d_tough_choices.txt > outputs\d_tough_choices.txt
+cmd /c gh.exe < inputs\e_so_many_books.txt > outputs\e_so_many_books.txt
+cmd /c gh.exe < inputs\f_libraries_of_the_world.txt > outputs\f_libraries_of_the_world.txt
